bc2a4651ec3f660bb7eb8f859d89dfaa122b76f2
godliang <2426534657@qq.com>
2021-05-24 15:28:56 +0800

    对象接收参数

--- ch03-recevieparam/ch03-recevieparam/src/main/java/com/bjpowernode/controller/MyController.java
+++ ch03-recevieparam/ch03-recevieparam/src/main/java/com/bjpowernode/controller/MyController.java
@@ -1,6 +1,7 @@
 package com.bjpowernode.controller;
 
 
+import com.bjpowernode.vo.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -68,5 +69,22 @@ public class MyController {
         mv.addObject("myage",age);
         mv.setViewName("show");
         return mv;
+    }
+
+    /**
+     *  处理器方法形参是java对象，这个对象的属性名和请求中参数名一样的
+     *  框架会创建形参的java对象，给属性赋值。请求中的参数时name，框架会调用setName()
+     */
+
+    @RequestMapping(value = "/receiveobject.do")
+    public ModelAndView receiveParam(Student myStudent){
+        System.out.println("receiveParam, name="+myStudent.getName()+"   age="+myStudent.getAge());
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("myname", myStudent.getName());
+        mv.addObject("myage",myStudent.getAge());
+        mv.addObject("mystudent", myStudent);
+        mv.setViewName("show");
+        return mv;
+
     }
 }
--- ch03-recevieparam/ch03-recevieparam/src/main/webapp/index.jsp
+++ ch03-recevieparam/ch03-recevieparam/src/main/webapp/index.jsp
@@ -18,6 +18,13 @@
         年龄：<input type="text" name="rage"><br/>
         <input type="submit" value="提交参数">
     </form>
+    <br/>
+    <p>使用java对象接收请求参数</p>
+    <form action="receiveobject.do" method="post">
+        姓名：<input type="text" name="name"><br/>
+        年龄：<input type="text" name="age"><br/>
+        <input type="submit" value="提交参数">
+    </form>
 
 </body>
 </html>
