fc5f484f2267c5944f73031e0aa2b9ef9c3457f0
godliang <2426534657@qq.com>
2021-05-27 15:12:23 +0800

    集中统一处理异常

---
+++ ch09-exception-handler/ch09-exception-handler/src/main/java/com/bjpowernode/exception/AgeException.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.exception;
+
+//当年龄有异常时，抛出的异常
+public class AgeException extends MyUserException {
+    public AgeException() {
+        super();
+    }
+
+    public AgeException(String message) {
+        super(message);
+    }
+}
